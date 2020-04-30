cf036857b775575c79e8734768890a8bbaaeded8
Will Leinweber <will@bitfission.com>
2020-04-29 18:32:58 -0700

    wip

--- keyboards/ergodox_infinity/keymaps/will/keymap.c
+++ keyboards/ergodox_infinity/keymaps/will/keymap.c
@@ -3,10 +3,11 @@
 #include "action_layer.h"
 #include "version.h"
 
-#define L0 0 // default layer
+#define LBASE 0 // default layer
 #define LWIND 1
 #define LSYMB 2
-#define L2 3
+#define LMDIA 3
+#define LGAME 4
 
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // can always be here
@@ -16,7 +17,7 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[L0] = LAYOUT_ergodox(  // layer 0 : default
+[LBASE] = LAYOUT_ergodox(  // layer 0 : default
 /*
  * left hand
  *    +-------+-----+-----+-----+-----+-----+-----+
@@ -30,19 +31,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *    +-+-----+-----+-----+-----+-----+-----+-----+
  *      |LALT |  `  |  \  |LEFT |RIGHT|
  *      +-----+-----+-----+-----+-----+   +-----+-----+
- *                                        | ESC |reset|
+ *                                        | ESC | lead|
  *                                  +-----+-----+-----+
  *                                  |     |     | HOME|
  *                                  |BKSPC| LGui+-----+
  *                                  |     |     | END |
  *                                  +-----+-----+-----+
  */
-        KC_EQL,        KC_1,   KC_2,    KC_3,    KC_4,    KC_5, LGUI(LSFT(KC_LBRC)),
-        KC_TAB,        KC_Q,   KC_W,    KC_E,    KC_R,    KC_T, TG(LSYMB),
-        KC_LCTL,       KC_A,   KC_S,    KC_D,    KC_F,    KC_G,
-        KC_LSFT,       KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, MO(LSYMB),
-KC_LALT, LT(KC_LALT,KC_GRV), KC_BSLS, KC_LEFT, KC_RGHT,
-                                                      KC_ESC, RESET,
+         KC_EQL,  KC_1,   KC_2,    KC_3,    KC_4,    KC_5, LGUI(LSFT(KC_LBRC)),
+         KC_TAB,  KC_Q,   KC_W,    KC_E,    KC_R,    KC_T, TG(LSYMB),
+         KC_LCTL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,
+         KC_LSFT, KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, MO(LSYMB),
+         KC_LALT, KC_GRV, KC_BSLS, KC_LEFT, KC_RGHT,
+                                                      KC_ESC, KC_LEAD,
                                                               KC_HOME,
                                             KC_BSPC, KC_LGUI, KC_END,
 /* right hand
@@ -51,11 +52,11 @@ KC_LALT, LT(KC_LALT,KC_GRV), KC_BSLS, KC_LEFT, KC_RGHT,
  *        +-----+-----+-----+-----+-----+-----+-------+
  *        | lck |  Y  |  U  |  I  |  O  |  P  |   \   |
  *        |  1  +-----+-----+-----+-----+-----+-------+
- *        +-----+  H  |  J  |  K  |  L  | ;/L2|   '   |
+ *        +-----+  H  |  J  |  K  |  L  |;/MDA|   '   |
  *        | f1  +-----+-----+-----+-----+-----+-------+
  *        |     |  N  |  M  |  ,  |  .  |  /  | LSYMB |
  *        +-----+-----+-----+-----+-----+-----+-----+-+
- *                    | UP  |DOWN | UP  |RIGHT| RALT|
+ *                    | UP  |DOWN |     |     | RALT|
  *    +-----+-----+   +-----+-----+-----+-----+-----+
  *    | DEL |RCTRL|
  *    +-----+-----+-----+
@@ -66,15 +67,38 @@ KC_LALT, LT(KC_LALT,KC_GRV), KC_BSLS, KC_LEFT, KC_RGHT,
  */
  LGUI(LSFT(KC_RBRC)), KC_6,   KC_7,    KC_8,    KC_9,   KC_0,             KC_MINS,
              TG(1),   KC_Y,   KC_U,    KC_I,    KC_O,   KC_P,             KC_BSLS,
-                      KC_H,   KC_J,    KC_K,    KC_L,   LT(L2, KC_SCLN),  KC_QUOT,
+                      KC_H,   KC_J,    KC_K,    KC_L,   LT(LMDIA, KC_SCLN),  KC_QUOT,
           MO(LSYMB),  KC_N,   KC_M,    KC_COMM, KC_DOT, KC_SLSH,          MO(LSYMB),
-                              KC_UP,   KC_DOWN, KC_UP,  KC_RIGHT,         KC_RALT,
+                              KC_UP,   KC_DOWN, KC_UP,  MO(LWIND),        KC_RALT,
              KC_DELETE, KC_RCTL,
              KC_PGUP,
              KC_PGDN, KC_ENT, KC_SPC
     ),
 
-[LSYMB] = LAYOUT_ergodox( // layer 1 : function and symbol layers
+[LWIND] = LAYOUT_ergodox( // layer 1 : windows
+    //  left hand
+       _______, _______, _______, _______, _______, _______, _______,
+       _______, _______, _______, _______, _______, _______, _______,
+       KC_LGUI, _______, _______, _______, _______, _______,
+       _______, _______, _______, _______, _______, _______, _______,
+       _______, _______, _______, _______, _______,
+                                           _______, _______,
+                                                    _______,
+                                  _______, KC_LCTL, _______,
+
+    // right hand
+       _______, _______, _______, _______, _______, _______, _______,
+       _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______,
+       _______, _______, _______, _______, _______, _______, _______,
+                         _______, _______, _______, _______, _______,
+       _______, _______,
+       _______,
+       _______, _______, _______
+    ),
+
+
+[LSYMB] = LAYOUT_ergodox( // layer 2 : function and symbol layers
 /* left hand
  *    +-------+-----+-----+-----+-----+-----+-----+
  *    | reset | f1  | f2  | f3  | f4  | f5  | f11 |
@@ -83,9 +107,9 @@ KC_LALT, LT(KC_LALT,KC_GRV), KC_BSLS, KC_LEFT, KC_RGHT,
  *    +-------+-----+-----+-----+-----+-----+     |
  *    |       |  #  |  $  |  (  |  )  |  `  +-----+
  *    +-------+-----+-----+-----+-----+-----+     |
- *    |       |  %  |  |  |  {  |  }  |  ^  |     |
+ *    |       |  =  |  |  |  {  |  }  |  ^  |     |
  *    +-+-----+-----+-----+-----+-----+-----+-----+
- *      |     |     |     |  <  |  >  |
+ *      |     |     |  %  |  <  |  >  |
  *      +-----+-----+-----+-----+-----+   +-----+-----+
  *                                        |     |     |
  *                                  +-----+-----+-----+
@@ -97,8 +121,8 @@ KC_LALT, LT(KC_LALT,KC_GRV), KC_BSLS, KC_LEFT, KC_RGHT,
        RESET  ,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F11,
        _______,KC_EXLM,KC_AT,  KC_LBRC,KC_RBRC,KC_TILD,_______,
        _______,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       _______,KC_PERC,KC_PIPE,KC_LCBR,KC_RCBR,KC_CIRC,_______,
-       BL_TOGG,BL_BRTG,BL_INC, BL_DEC, _______,
+       _______,KC_EQL, KC_PIPE,KC_LCBR,KC_RCBR,KC_CIRC,_______,
+       _______,_______,KC_PERC,KC_LABK,KC_RABK,
                                        _______,_______,
                                                KC_HOME,
                                _______,_______,KC_END,
@@ -130,7 +154,7 @@ KC_LALT, LT(KC_LALT,KC_GRV), KC_BSLS, KC_LEFT, KC_RGHT,
        _______,
        _______, _______, KC_KP_0
 ),
-[L2] = LAYOUT_ergodox(
+[LMDIA] = LAYOUT_ergodox(
 /* Left hand
  *    +-------+-----+-----+-----+-----+-----+-----+
  *    | reset |     |     |     |     |     |     |
@@ -139,7 +163,7 @@ KC_LALT, LT(KC_LALT,KC_GRV), KC_BSLS, KC_LEFT, KC_RGHT,
  *    +-------+-----+-----+-----+-----+-----+  2  |
  *    |       |     |MsLft|MsDwn|MsRgt|     +-----+
  *    +-------+-----+-----+-----+-----+-----+     |
- *    |       |     |     |     |     |     |     |
+ *    |       |     |BLTOG|BLINC|BLDEC|     |     |
  *    +-+-----+-----+-----+-----+-----+-----+-----+
  *      |     |     |     |     |     |
  *      +-----+-----+-----+-----+-----+   +-----+-----+
@@ -151,9 +175,9 @@ KC_LALT, LT(KC_LALT,KC_GRV), KC_BSLS, KC_LEFT, KC_RGHT,
  *                                  +-----+-----+-----+
  */
        RESET,   _______, _______, _______, _______, _______, _______,
-       _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, TG(L2),
+       _______, _______, KC_BTN1, KC_MS_U, KC_BTN2, _______, TG(LSYMB),
        _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______,
-       _______, _______, _______, _______, _______, _______, _______,
+       _______, _______, BL_TOGG, BL_INC,  BL_DEC,  _______, _______,
        _______, _______, _______, _______, _______,
                                            _______, _______,
                                                     _______,
@@ -178,7 +202,7 @@ KC_LALT, LT(KC_LALT,KC_GRV), KC_BSLS, KC_LEFT, KC_RGHT,
  *    +-----+-----+-----+
  */
        _______, _______,  _______, _______, _______, _______, RESET,
-       TG(L2),  _______,  _______, _______, _______, _______, _______,
+    TG(LSYMB),  _______,  _______, _______, _______, _______, _______,
                 KC_COLON, KC_MPLY, KC_MPRV, KC_MNXT, _______, _______,
        _______, _______,  KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,
                           _______, _______, _______, _______, _______,
@@ -186,6 +210,48 @@ KC_LALT, LT(KC_LALT,KC_GRV), KC_BSLS, KC_LEFT, KC_RGHT,
        _______,
        _______, _______, _______
 ),
+
+[LGAME] = LAYOUT_ergodox( // layer 3 : games
+/*
+ * left hand
+ *    +-------+-----+-----+-----+-----+-----+-----+
+ *    |  ESC  |  1  |  2  |  3  |  4  |  5  |  6  |
+ *    +-------+-----+-----+-----+-----+-----+-----+
+ *    |  TAB  |  I  |  Q  |  W  |  E  |  R  |  T  |
+ *    +-------+-----+-----+-----+-----+-----+     |
+ *    | Ctrl  |  T  |  A  |  S  |  D  |  F  +-----+
+ *    +-------+-----+-----+-----+-----+-----+  G  |
+ *    | LSHFT |  Z  |  X  |  C  |  V  |  B  |     |
+ *    +-+-----+-----+-----+-----+-----+-----+-----+
+ *      |LGUI |  J  |  U  |  Y  |  O  |
+ *      +-----+-----+-----+-----+-----+   +-----+-----+
+ *                                        | P   | tog |
+ *                                  +-----+-----+-----+
+ *                                  |     |     |  K  |
+ *                                  |SPACE| Ctrl+-----+
+ *                                  |     |     |SHIFT|
+ *                                  +-----+-----+-----+
+ */
+    KC_EQL,  KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,
+    KC_TAB,  KC_I, KC_Q, KC_W, KC_E, KC_R, KC_T,
+    KC_LCTL, KC_T, KC_A, KC_S, KC_D, KC_F,
+    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_G,
+    KC_LGUI, KC_J, KC_U, KC_Y, KC_O,
+                                                KC_P, TG(LGAME),
+                                                      KC_K,
+                                     KC_SPC, KC_LCTL, KC_LSFT,
+
+    // right hand
+       _______, _______, _______, _______, _______, _______, _______,
+       _______, _______, _______, _______, _______, _______, _______,
+                _______, _______, _______, _______, _______, _______,
+       _______, _______, _______, _______, _______, _______, _______,
+                         _______, _______, _______, _______, _______,
+       _______, _______,
+       _______,
+       _______, _______, _______
+    ),
+
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
@@ -238,12 +304,32 @@ void matrix_init_user(void) {
 
 };
 
+LEADER_EXTERNS();
 
 // Runs constantly in the background, in a loop.
 void matrix_scan_user(void) {
 
-    uint8_t layer = biton32(layer_state);
+  LEADER_DICTIONARY() {
+    leading = false;
+    leader_end();
+    SEQ_ONE_KEY(KC_F) {
+      SEND_STRING("QMK is awesome!");
+    }
 
+    SEQ_ONE_KEY(KC_W) {
+      layer_invert(LWIND);
+    }
+
+    SEQ_ONE_KEY(KC_G) {
+      layer_invert(LGAME);
+    }
+
+  }
+
+
+    //uint8_t layer = biton32(layer_state);
+
+  /**
     ergodox_board_led_off();
     ergodox_right_led_1_off();
     ergodox_right_led_2_off();
@@ -260,5 +346,6 @@ void matrix_scan_user(void) {
             // none
             break;
     }
+    */
 
 };
